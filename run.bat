b918258e461aed763ad1964f2ecc167806c981b3
Kokomi <68575524+Kokomi-BC@users.noreply.github.com>
2025-11-25 11:29:21 +0800

    更新 run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 @echo off
 chcp 65001 >nul
 
-:: 设置项目目录
-set "PROJECT_DIR=C:\Users\Kokomi\Desktop\new\Detect"
+:: 设置项目目录为当前批处理文件所在目录
+set "PROJECT_DIR=%~dp0"
 
-echo 正在切换到项目目录: %PROJECT_DIR%
+echo 当前项目目录: %PROJECT_DIR%
 cd /d "%PROJECT_DIR%"
 
 echo 正在执行 npm run build...
