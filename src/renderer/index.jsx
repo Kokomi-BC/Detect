a32884b087bee711b3c26dcd75713cf22b8c036a
Kokomi <kokomi_123@qq.com>
2025-11-25 18:48:09 +0800

    bug fix

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '@chatui/core/dist/index.css';
 import 'mdui/mdui.css';
 import { setColorScheme } from 'mdui';
 
